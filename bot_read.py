d1b9750634e6efbdfda33466ae1f47584eacd906
Claudio Castello <claudio.chemist@gmail.com>
2021-03-10 19:59:29 -0300

    The bot can read now
    Set the bot config to praw.ini
    Created bot_read.py

---
+++ bot_read.py
@@ -0,0 +1,13 @@
+import praw
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("FibonacciAsFuck")
+
+submission = subreddit.hot(limit=5)
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
